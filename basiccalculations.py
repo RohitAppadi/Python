5b9da65b14f0ca44b9d78ada61761ae0669914d2
ARohit <132917495+RohitAppadi@users.noreply.github.com>
2025-11-22 20:43:59 +0530

    Add files via upload

---
+++ basiccalculations.py
@@ -0,0 +1,22 @@
+#varibles 
+a = int(input( "Enter first number: " ))
+b = int(input( "Enter second number: " ))
+#addition
+sum = a + b
+print("The sum is:", sum) 
+
+#subtraction
+difference = a - b
+print("The difference is:", difference)
+
+#multiplication
+product = a * b
+print("The product is:", product)
+
+#division
+quotient = a / b
+print("The quotient is:", quotient)
+
+reminder = a % b
+print("The reminder is:", reminder)
+
